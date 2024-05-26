761234b0669b7a3baf9336a90cc9307a863dc29f
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-05-26 20:23:53 -0300

    add toaster to main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
+// import { Lato } from "next/font/google";
 // import { Providers } from "./providers";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
-const lato = Lato({ subsets: ["latin"], weight: "400" });
+// const lato = Lato({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Alline Duarte",
@@ -18,9 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background", lato.className)}>
+      <body className={cn("min-h-screen bg-background")}>
         {/* <Providers>{children}</Providers> */}
         {children}
+        <Toaster />
       </body>
     </html>
   );
