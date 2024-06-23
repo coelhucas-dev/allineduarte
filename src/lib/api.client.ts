fcb7702ab331be4765f99f2ee50b45fb38811ec3
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 10:43:29 -0300

    feat: create headers with api authorization

--- src/lib/api.client.ts
+++ src/lib/api.client.ts
@@ -1,17 +1,54 @@
 import axios, { AxiosResponse } from "axios";
-import { env } from "./env";
 import { cache } from "react";
-import { ISchedule, IClinic } from "@/types";
+import { env } from "./env";
+import { ISchedule, IClinic, IPatient, IPatientResponse } from "@/types";
+
+const defaultHeaders = {
+  Authorization: `Basic ${btoa(
+    `${env.TECH_USER_USERNAME}:${env.TECH_USER_PASSWORD}`,
+  )}`,
+};
 
 export const axiosClient = axios.create({
-  baseURL: env.API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
   timeout: 10000,
+  headers: defaultHeaders,
 });
 
 export const fetchClinicsApi = cache(async (): Promise<IClinic[]> => {
-  const res: AxiosResponse<IClinic[]> = await axiosClient.get("/clinic");
+  const res: AxiosResponse<IClinic[]> = await axiosClient.get("/clinic/");
   return res.data;
 });
+
+export const createPatientApi = async (
+  req: IPatient,
+): Promise<{
+  message: string;
+  response: IPatientResponse;
+}> => {
+  const res: AxiosResponse<IPatientResponse> = await axiosClient.post(
+    "/patient/",
+    req,
+  );
+  return {
+    message: "Success on creating patient",
+    response: res.data,
+  };
+};
+
+export const getScheduledApi = async (): Promise<{
+  message: string;
+  response: any;
+}> => {
+  const res: AxiosResponse = await axiosClient.get(
+    "/appointment/get_scheduled/",
+  );
+
+  return {
+    message: "Success on getting scheduled appointments",
+    response: res.data,
+  };
+};
 
 export const scheduleAppointmentApi = async (
   req: ISchedule,
@@ -24,7 +61,7 @@ export const scheduleAppointmentApi = async (
     req,
   );
   return {
-    message: res.data,
-    response: "Success on creating appointment",
+    message: "Success on creating appointment",
+    response: res.data,
   };
 };
