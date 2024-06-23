e0faea911b6f18757354df54703f6b4e706cbc28
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 10:44:34 -0300

    update: deps and env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    TECH_USER_USERNAME: process.env.TECH_USER_USERNAME,
+    TECH_USER_PASSWORD: process.env.TECH_USER_PASSWORD,
+  },
+};
 
 export default nextConfig;
