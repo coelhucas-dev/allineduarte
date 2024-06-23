48a7dd25c8f57d4643bc3de28f952b5dbbad28b2
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 10:43:08 -0300

    fix: env not loaded

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,20 +1,23 @@
-import z from "zod";
+import z from 'zod';
 
 const envSchema = z.object({
-  API_BASE_URL: z.string().url().default("http://localhost:8000"),
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
+  API_BASE_URL: z.string(),
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+  TECH_USER_USERNAME: z.string(),
+  TECH_USER_PASSWORD: z.string(),
 });
 
 const envServer = envSchema.safeParse({
   NODE_ENV: process.env.NODE_ENV,
   API_BASE_URL: process.env.API_BASE_URL,
+  TECH_USER_USERNAME: process.env.TECH_USER_USERNAME,
+  TECH_USER_PASSWORD: process.env.TECH_USER_PASSWORD,
 });
 
 if (!envServer.success) {
-  console.log(envServer);
-  console.error(envServer.error.issues);
+  console.log("Failed to parse environment variables:");
+  console.error(envServer.error.format()); // Log detailed error messages
+
   throw new Error("There is an error with the server environment variables");
 }
 
