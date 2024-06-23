9e2f4daed5217c6d7af5868c7ffd5a12951c414f
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 10:44:12 -0300

    update: change margin for main background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Header from "@/components/header";
 export default function Home() {
   return (
     <main className="overflow-x-hidden m-auto relative md:max-w-[1440px] shadow-lg">
-      <div className="bg-main overflow-x-hidden inset-0 mt-[-240px] md:mt-[-250px] ml-[-100px] md:ml-[-400px] z-auto lg:w-[1900px]  md:w-[1500px] w-[900px] h-[700px] md:h-[800px] absolute rotate-[-12deg] top-0 bg-gradient-to-r from-[#265d58] to-[#6d9c99] md:from-[#265d58] md:to-[#467975] lg:to-[#497c79]"></div>
+      <div className="bg-main overflow-x-hidden inset-0 mt-[-240px] md:mt-[-150px] ml-[-100px] md:ml-[-400px] z-auto lg:w-[1900px]  md:w-[1500px] w-[900px] h-[700px] md:h-[800px] absolute rotate-[-12deg] top-0 bg-gradient-to-r from-[#265d58] to-[#6d9c99] md:from-[#265d58] md:to-[#467975] lg:to-[#497c79]"></div>
       <Header />
-      <div className="flex min-h-screen flex-col items-center justify-between px-4 py-8 z-1 bg-white shadow-lg">
+      <div className="flex min-h-screen flex-col items-center justify-between px-4 py-8 z-1 bg-white shadow-lg md:mt-12">
         <section className="container text-center z-20">
           <h1 className="font-['Marcellus_SC'] font-[400] text-center text-[30px] text-nowrap text-white mt-3 md:text-[42px]">
             Dra. Alline Duarte
